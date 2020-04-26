70f3bccbcc1886afcb2f33db031646ee70f3c7fc
zack-bitcoin <zack.bitcoin@gmail.com>
2020-04-26 00:38:42 -0400

    added comments to the hashtable code

--- hashtable.pl
+++ hashtable.pl
@@ -18,6 +18,7 @@ hash(K, K2) :-
     K2 is (K * Prime) mod Prime2.
 
 key2path(K, P) :-
+    %This is converting K to binary.
     hash(K, K2),
     path_size(Path),
     key2path(K2, P, Path).
@@ -29,31 +30,33 @@ key2path(K, [H|T], N) :-
     key2path(K2, T, M).
 
 add(K,V,T,U) :-
+    %store a key-value pair in the dictionary.
     key2path(K, P),
     add2(P, V, T, U).
 
+%an empty tree is 'nil', so this is adding the first element to a new tree.
 add2(K,V,nil,leaf(K, V)).
 
+%a tree with just one element is a 'leaf'. so this is how we add our second element to a tree.
+%if our new leaf is following the same path as an existing one
 add2([0|K],V,leaf([0|K2], V2), stem(S, nil)) :-
     add2(K, V, leaf(K2, V2), S).
 add2([1|K],V,leaf([1|K2], V2), stem(nil, S)) :-
     add2(K, V, leaf(K2, V2), S).
+%if our new leaf is splitting away from the path of the existing one
 add2([0|K],V,leaf([1|K2], V2),
     stem(leaf(K, V), leaf(K2, V2))).
 add2([1|K],V,leaf([0|K2], V2),
     stem(leaf(K2, V2), leaf(K, V))).
 
-add2([0|K], V, stem(nil, B),
-    stem(leaf(K, V), B)).
-add2([1|K], V, stem(A, nil),
-    stem(A, leaf(K, V))).
-
+%this is how we add an element to a tree with more than 2 elements.
 add2([0|K], V, stem(A, B), stem(S, B)) :-
     add2(K, V, A, S).
 add2([1|K], V, stem(A, B), stem(A, S)) :-
     add2(K, V, B, S).
     
 get(K, T, V) :-
+    %use a key to look up a value.
     key2path(K, P),
     get2(P, T, V).
 get2(P, leaf(P, V), V).
