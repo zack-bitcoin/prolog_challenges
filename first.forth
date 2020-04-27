fb1fed2c37f111fe001fe4e3e6b87502ac738e0c
zack-bitcoin <zack.bitcoin@gmail.com>
2020-04-27 00:39:03 -0400

    wrote factorial in forth

--- first.forth
+++ first.forth
@@ -1,4 +1,10 @@
-23 4 dup >r + r>
+% 23 4 dup >r + r>
 : double dup + ;
 
-double double double
+% double double double
+
+
+: factorial 1 swap factorial2 ;
+: factorial2 dup if dup >r * r> 1 - factorial2 else drop then ;
+
+10 factorial
--- forth.pl
+++ forth.pl
@@ -29,6 +29,18 @@ process_word('r>', S, [A|R], HT, Funs,
 process_word('+', [A|[B|S]], R, HT, Funs,
              [C|S], R, HT, Funs, _) :-
     C is A + B.
+process_word('*', [A|[B|S]], R, HT, Funs,
+             [C|S], R, HT, Funs, _) :-
+    C is A * B.
+process_word('div', [A|[B|S]], R, HT, Funs,
+             [C|S], R, HT, Funs, _) :-
+    C is B div A.
+process_word('-', [A|[B|S]], R, HT, Funs,
+             [C|S], R, HT, Funs, _) :-
+    C is B - A.
+process_word('mod', [A|[B|S]], R, HT, Funs,
+             [C|S], R, HT, Funs, _) :-
+    C is B mod A.
 process_word(':', S, R, HT, Funs,
              S, R, HT, Funs2, In) :-
     get_function(In, Funs, Funs2).
@@ -39,7 +51,7 @@ process_word(Word, S, R, HT, Funs,
     S2 = [N|S].
 process_word(Word, S, R, HT, Funs,
              S2, R2, HT2, Funs2, In) :-
-    %check if we are calling a function.
+    %check if this word is a defined function.
     numberify_atom(Word, Name),
     get(Name, Funs, Value),
     not(Value == empty),
@@ -47,6 +59,18 @@ process_word(Word, S, R, HT, Funs,
 
 
 process_words([], S, R, HT, Funs, S, R, HT, Funs, _).
+process_words([then|T], S, R, HT, Funs, S2, R2, HT2, Funs2, In) :-
+    process_words(T, S, R, HT, Funs, S2, R2, HT2, Funs2, In).
+process_words([if|T], [0|S], R, HT, Funs, S2, R2, HT2, Funs2, In) :-
+    !,
+    tillX_list(else, T, T2),
+    process_words(T2, S, R, HT, Funs, S2, R2, HT2, Funs2, In).
+process_words([if|T], [_|S], R, HT, Funs, S2, R2, HT2, Funs2, In) :-
+    process_words(T, S, R, HT, Funs, S2, R2, HT2, Funs2, In).
+process_words([else|T], S, R, HT, Funs, S2, R2, HT2, Funs2, In) :-
+    !,
+    tillX_list(then, T, T2),
+    process_words(T2, S, R, HT, Funs, S2, R2, HT2, Funs2, In).
 process_words([Word|T], S, R, HT, Funs, S2, R2, HT2, Funs2, In) :-
     process_word(Word, S, R, HT, Funs, S3, R3, HT3, Funs3, In),
     process_words(T, S3, R3, HT3, Funs3, S2, R2, HT2, Funs2, In).
@@ -67,7 +91,7 @@ get_function3(In, Fun, Result, Word) :-
     append(Fun,[Word],Fun2),
     get_function2(In, Fun2, Result).
     
-
+%can this atom be converted to a number?
 can_number(A) :-
     atom_chars(A, L),
     can_number2(L).
@@ -78,6 +102,7 @@ can_number2([H|T]) :-
     N < 58,
     can_number2(T).
 
+%the hashtable can only store by integer key. So we need a way to deterministically convert atoms into integers, and we don't want different atoms to collide with the same integer.
 numberify_atom(A, N) :-
     atom_codes(A, L),
     numberify_atom2(1, L, N).
@@ -103,13 +128,34 @@ get_word2(In, '\n', '', Result) :-
 get_word2(In, '\t', '', Result) :-
     !,
     get_word(In, '', Result).
+get_word2(In, '%', '', Result) :-
+    !,
+    tillX('\n', In),
+    get_word(In, '', Result).
 
 get_word2(_, ' ', Word, Word) :- !.
 get_word2(_, '\n', Word, Word) :- !.
 get_word2(_, '\t', Word, Word) :- !.
+get_word2(In, '%', Word, Word) :-
+    !,
+    tillX('\n', In).
 
 get_word2(In, C, Word, Result) :-
     get_char(In, C2),
     atom_concat(Word, C, Word2),
     get_word2(In, C2, Word2, Result).
+
+
+tillX(X, In) :-
+    tillX(X, In, ' ').
+tillX(X, _, X).
+tillX(_, _, end_of_file).
+tillX(X, In, _) :-
+    get_char(In, C),
+    tillX(X, In, C).
+
+tillX_list(X, [X|R], R).
+tillX_list(X, [_|T], R) :-
+    tillX_list(X, T, R).
+    
     
